cbf3feceff76e00176a251e8f26c2e8172289698
Alexandr Tretyakov <alex@tretyakov.net>
2021-08-27 14:07:09 +0300

    Добавил занесение расходов в базу

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 import telebot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 from messages import text_messages
 from models import *
@@ -12,6 +12,9 @@ db.connect()
 db.create_tables([User, Category, Expense])
 
 bot = telebot.TeleBot(os.environ.get('TOKEN'))
+
+category = ''
+user = ''
 
 
 def gen_main_keyboard():
@@ -28,6 +31,40 @@ def send_welcome(message):
     """Отправляем приветственное сообщение и помощь по работе с ботом"""
     name = message.from_user.first_name
     bot.send_message(message.chat.id, text_messages['welcome'].format(name=name), reply_markup=gen_main_keyboard())
+
+
+@bot.message_handler(func=lambda msg: msg.text == 'Внести расход' + u'\U0001F4B0')
+def send_category(message):
+    """Вывод выбора категории"""
+    global user
+
+    user = User.get_or_create(telegram_id=message.from_user.id)[0]
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+
+    categories = Category.select()
+
+    for category in categories:
+        markup.add(InlineKeyboardButton(category.name, callback_data=f'category__{category._pk}'))
+
+    bot.send_message(message.chat.id, text_messages['select_category'], reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('category'))
+def callback_query(call):
+    """Получение суммы расхода от пользователя"""
+    global category
+    category = call.data.split("__")[1]
+
+    msg = bot.send_message(call.message.chat.id, text_messages['enter_amount'])
+    bot.register_next_step_handler(msg, make_expense)
+
+
+def make_expense(message):
+    """Занесение расхода в базу"""
+    Expense.create(category_id=category, user_id=user, amount=message.text)
+
+    bot.send_message(message.chat.id, text_messages['enter_amount_success'], reply_markup=gen_main_keyboard())
 
 
 bot.polling()
