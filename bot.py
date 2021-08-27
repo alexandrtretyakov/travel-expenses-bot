e34aa97a71a5b6760ccf74a5185b223d5c4625d0
Alexandr Tretyakov <alex@tretyakov.net>
2021-08-27 16:48:36 +0300

    Добавил вывод статистики

--- bot.py
+++ bot.py
@@ -33,6 +33,8 @@ def send_welcome(message):
     bot.send_message(message.chat.id, text_messages['welcome'].format(name=name), reply_markup=gen_main_keyboard())
 
 
+# Расходы
+
 @bot.message_handler(func=lambda msg: msg.text == 'Внести расход' + u'\U0001F4B0')
 def send_category(message):
     """Вывод выбора категории"""
@@ -65,6 +67,73 @@ def make_expense(message):
     Expense.create(category_id=category, user_id=user, amount=message.text)
 
     bot.send_message(message.chat.id, text_messages['enter_amount_success'], reply_markup=gen_main_keyboard())
+
+
+# Статистика
+
+@bot.message_handler(func=lambda msg: msg.text == 'Статистика' + u'\U0000231B')
+def send_category(message):
+    """Выбор периода для статистики"""
+    global user
+
+    user = User.get_or_create(telegram_id=message.from_user.id)[0]
+
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add(InlineKeyboardButton('За сегодня', callback_data='today'))
+    markup.add(InlineKeyboardButton('За всю поездку', callback_data='trip'))
+
+    bot.send_message(message.chat.id, text_messages['select_statistics_period'], reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'today')
+def callback_query(call):
+    """Вывод статистики за день"""
+    qs = Expense. \
+        select(Category.name, fn.SUM(Expense.amount)). \
+        join(Category). \
+        where((Expense.created_at > datetime.datetime.today().strftime('%Y-%m-%d')) &
+              (Expense.user_id == user)). \
+        group_by(Category.name)
+
+    message = u'Расходы сегодня:\n\n'
+
+    for q in qs:
+        message += u'{}: {} руб.\n'.format(q.category_id.name, q.amount)
+
+    query = Expense. \
+        select(fn.SUM(Expense.amount)). \
+        where((Expense.created_at > datetime.datetime.today().strftime('%Y-%m-%d')) &
+              (Expense.user_id == user))
+    sum = query.scalar()
+
+    message += u'\nВсего израсходовано: {} рублей'.format(sum)
+
+    bot.send_message(call.message.chat.id, message, reply_markup=gen_main_keyboard())
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'trip')
+def callback_query(call):
+    """Вывод статистики за поездку"""
+    qs = Expense. \
+        select(Category.name, fn.SUM(Expense.amount)). \
+        join(Category). \
+        where(Expense.user_id == user). \
+        group_by(Category.name)
+
+    message = u'Расходы за весь отпуск:\n\n'
+
+    for q in qs:
+        message += u'{}: {} руб.\n'.format(q.category_id.name, q.amount)
+
+    query = Expense. \
+        select(fn.SUM(Expense.amount)). \
+        where(Expense.user_id == user)
+    sum = query.scalar()
+
+    message += u'\nВсего израсходовано: {} рублей'.format(sum)
+
+    bot.send_message(call.message.chat.id, message, reply_markup=gen_main_keyboard())
 
 
 bot.polling()
